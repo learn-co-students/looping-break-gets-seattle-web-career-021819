49de940df6b230f0cce0c6b5a28ac31db09ce4cc
github username <github email address>
2019-02-01 10:08:25 -0800

    looping break lab solution

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
-	#your code here
+  loop do
+	  puts "What is the spell that enacts levitation?"
+    input = gets.chomp
+    break if input == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
 end
-
-
